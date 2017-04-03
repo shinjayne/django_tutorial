5b908fbd6fc2eab3d86782b710dca161b2ae9eba
Shin Jayne <shinjayne@Shinui-MacBook-Pro.local>
2017-04-04 02:04:49 +0900

    admin 에 모델을 등록하는 세가지 방법

--- blog/admin.py
+++ blog/admin.py
@@ -3,3 +3,11 @@ from .models import Post
 
 # Register your models here.
 admin.site.register(Post)
+
+admin.site.unregister(Post)
+
+# 등록법 2
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content']
+
+admin.site.register(Post, PostAdmin)
--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from polls.models import Question, Choice
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text', 'pub_date']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.unregister(Question)
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text', 'pub_date']
+
+
+
 admin.site.register(Choice)
