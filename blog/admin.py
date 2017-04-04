f703e750183c64e0bfbce649f1fc6866f86332b6
Shin Jayne <shinjayne@Shinui-MacBook-Pro.local>
2017-04-05 01:48:10 +0900

    blog/admin.py 에 ModelAdmin 이용 - action 옵션 사용하기

--- blog/admin.py
+++ blog/admin.py
@@ -12,22 +12,30 @@ class PostAdmin(admin.ModelAdmin):
     #ModelAdmin 의 option : list_display
     list_display = ['id', 'title', 'content_size', 'created_at', 'updated_at']
 
+
     #list_display 의 customizing
     def content_size(self, post):
-        return mark_safe('<strong>{}</strong>글자'.format(len(post.content))
-
+        return mark_safe('<strong>{}</strong>글자'.format(len(post.content)))
     content_size.short_description = '글자수'
+
 admin.site.register(Post, PostAdmin)
-admin.site.unregister(Post, PostAdmin)
+admin.site.unregister(Post)
 
 # 등록법 3
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     #ModelAdmin 의 option : list_display
-    list_display = ['id', 'title', 'content_size', 'created_at', 'updated_at']
-
+    list_display = ['id', 'title', 'content_size','status', 'created_at', 'updated_at']
+    #ModelAdmin 의 option : Actions
+    actions = ['make_published']
     #list_display 의 customizing
     def content_size(self, post):
-        return mark_safe('<strong>{}</strong>글자'.format(len(post.content))
-
+        return mark_safe('<strong>{}</strong>글자'.format(len(post.content)))
     content_size.short_description = '글자수'
+
+    #Actions의 커스터마이징
+    def make_published(self, request, queryset):
+        updated_count = queryset.update(status='p') #QuerySet.update --뒷부분에서 자세히 다룸
+        self.message_user(request, '{}건의 포스팅을 Publisehd 상태로 변경'.format(updated_count))
+        #django message framework -- 뒷부분에서 자세히 다룰 예정
+    make_published.short_description = '선택된 항목의 status를 published로 변경합니다.'
